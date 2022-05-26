3d8af216c254fe61b5794f53841c32a04cdead24
Al-Amin931761 <alamin931761@gmail.com>
2022-05-27 00:48:22 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,12 +11,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0oqfs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyToken(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: '401 - Unauthorized access' })
+    }
+    const accessToken = authorization.split(' ')[1];
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: '403 - Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
         await client.connect();
         const partsCollection = client.db('computer_parts').collection('parts');
         const bookingCollection = client.db('computer_parts').collection('booking');
+        const userCollection = client.db('computer_parts').collection('users');
 
         // all parts data load
         app.get('/parts', async (req, res) => {
@@ -31,19 +47,41 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const part = await partsCollection.findOne(query);
             res.send(part);
-        })
+        });
 
+        // booked all data load 
         app.post("/booking", async (req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
-        })
+        });
 
-        app.get('/booking', async (req, res) => {
+        // booked single data load
+        app.get('/booking', verifyToken, async (req, res) => {
             const buyerEmail = req.query.buyerEmail;
-            const query = { buyerEmail: buyerEmail }
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (buyerEmail === decodedEmail) {
+                const query = { buyerEmail: buyerEmail }
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({ message: '403 - Forbidden access' });
+            }
+        });
+
+        // add and update user 
+        app.put('/user/:email', async (req, res) => {
+            const user = req.body;
+            const email = req.params.email;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         })
 
     }
